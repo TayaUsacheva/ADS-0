73009d67cbd73ab00c4b0adfcd453c882f2bb53e
TayaUsacheva <116427632+TayaUsacheva@users.noreply.github.com>
2023-02-26 14:35:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,13 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    if(a != b){
-        if(a > b)
+    if (a != b) {
+        if (a > b) {
             return gcd(a - b, b);
-        else 
+        } else {
             return gcd(a, b - a);
-    }
-    else{
+        }
+    } else {
         return a;
     }
 }
